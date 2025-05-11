77498cccb34bce9073282c99358fec9f4bfb08fc
CodesJosh <134979823+CodesJosh@users.noreply.github.com>
2025-05-11 19:31:37 -0400

    Delete LosPlataformas directory

--- LosPlataformas/routes/verificador.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const verificadorController = require('../controllers/verificadocontroller');
-
-router.post('/login', verificadorController.loginUsuario);
-
-module.exports = router;
